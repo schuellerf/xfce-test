9975d4317b5fd0c5ee973f8140796992fe0f00ef
Florian Schüller <florian.schueller@gmail.com>
2017-03-22 23:15:07 +0100

    Implement reusing of Scenarios

---
+++ behave/environment.py
@@ -0,0 +1,6 @@
+def before_scenario(context, scenario):
+    """ saves all scenarios for later use
+    """
+    if "all_my_scenarios" not in context._root:
+        context._root["all_my_scenarios"] = {}
+    context._root["all_my_scenarios"][scenario.name.lower()] = scenario.all_steps
---
+++ behave/steps/reuse.py
@@ -0,0 +1,23 @@
+import behave
+
+def _reTextify(thing):
+    """ return text of step or scenario as if it was
+        in a feature file
+    """
+    if type(thing) is behave.model.Scenario:
+	return unicode(thing.name)
+    if type(thing) is behave.model.Step:
+	return u"{0} {1}".format(thing.step_type,thing.name)
+    raise NotImplementedError(u'I don\'t know how to _reTextify ' + str(type(thing)))
+
+def _get_scenario(context, scenario):
+    """ return string representation of given scenario
+    """
+    ret = ""
+    for step in context._root["all_my_scenarios"][scenario.lower()]:
+        ret += _reTextify(step) + u"\n"
+    return ret
+
+@given(u'I repeat "{scenario}"')
+def step_impl(context, scenario):
+    context.execute_steps(_get_scenario(context, scenario))
