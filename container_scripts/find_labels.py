fccf5d7300b6c84ba7a8eeec1fb6694f158e302a
Florian Schüller <florian.schueller@gmail.com>
2019-04-16 14:16:23 +0200

    backup: minor improvement, leaving the identifier at the end

--- container_scripts/find_labels.py
+++ container_scripts/find_labels.py
@@ -8,6 +8,7 @@ img_name = l.imagecapture()
 
 wins=l.getwindowlist()
 
+re_pattern = r"(.*)auto([0-9]+)auto(.*)"
 
 for w in wins:
     obj = l.getobjectlist(w)
@@ -15,14 +16,16 @@ for w in wins:
         info = l.getobjectinfo(w,o)
         if 'label' in info:
             label = l.getobjectproperty(w,o,'label')
-            m = re.search("auto([0-9]+)auto", label)
+            print(w,o)
+            m = re.search(re_pattern, label)
             if m:
+                id_num = m.group(2)
                 size = l.getobjectsize(w,o)
                 if size[0] > 0:
-                    print("Translation #{} is here: ('{}','{}')".format(m.group(1), w, o))
+                    print("Translation #{} is here: ('{}','{}')".format(id_num, re.sub(re_pattern,"\\1\\3",w), re.sub(re_pattern,"\\1\\3",o)))
                     print("Located in picture: {}".format(size))
                     img = cv2.imread(img_name)
                     new_img = cv2.rectangle(img, (size[0], size[1]), (size[0] + size[2], size[1] + size[3]), (0,0,255), 3)
-                    cv2.imwrite('/tmp/translation_{}_{}.png'.format(m.group(1), o), new_img)
+                    cv2.imwrite('/tmp/translation_{}_{}.png'.format(id_num, o), new_img)
 
 
--- container_scripts/patch_automate_po.py
+++ container_scripts/patch_automate_po.py
@@ -3,7 +3,9 @@ import re
 i=0
 with open("po/en_GB.po", 'r') as in_file, open("po/automate.po", 'w') as out_file:
     for line in in_file:
-        line = re.sub(r"msgstr \"([^<].+)\"", r'msgstr "auto{}auto\1"'.format(i), line)
-        line = re.sub(r"msgstr \"(<[^>]+>)(.+)\"", r'msgstr "\1auto{}auto\2"'.format(i), line)
-        i+=1
+        orig_line = line
+        line = re.sub(r"msgstr \"([^<].+)\"", r'msgstr "\1auto{}auto"'.format(i), line)
+        line = re.sub(r"msgstr \"(<[^>]+>)(.+)(<[^>]+>)\"", r'msgstr "\1\2auto{}auto\3"'.format(i), line)
+        if line != orig_line:
+            i += 1
         out_file.write(line)
