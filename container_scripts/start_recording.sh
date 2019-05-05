b9f7b2224d8b876c552a60855675b7a3bcc6d788
Florian Schüller <florian.schueller@gmail.com>
2019-05-05 15:37:47 +0200

    Improve video recording scripts

--- container_scripts/start_recording.sh
+++ container_scripts/start_recording.sh
@@ -1,5 +1,10 @@
 #!/bin/env bash
 
 RESOLUTION=${RESOLUTION:-800x600}
+DATE=$(date +%Y%m%d_%H%M%S)
+OVERLAY_FILE=${OVERLAY_FILE:-/tmp/video.txt}
 
-ffmpeg -y -r 30 -f x11grab -s ${RESOLUTION} -i :99.0 -vf "drawtext=fontfile=Vera.ttf:textfile=/tmp/video.txt:reload=1:fontcolor=white: fontsize=12: box=1: boxcolor=black@0.5:y=500" -c:v libx264 -f mpegts - 2>/data/video_log > /data/video.ts &
+# assure file is existing and there is at least a newline
+echo "" > ${OVERLAY_FILE}
+
+ffmpeg -y -r 30 -f x11grab -s ${RESOLUTION} -i ${DISPLAY} -vf "drawtext=fontfile=Vera.ttf:textfile=${OVERLAY_FILE}:reload=1:fontcolor=white: fontsize=12: box=1: boxcolor=black@0.5:y=500" -c:v libx264 -f mpegts - 2>/data/video_${DATE}.log > /data/video_${DATE}.ts &
