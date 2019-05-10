a96f955fe9f6ebe607ab03091e8b3dcfe45f21c7
Florian Schüller <florian.schueller@gmail.com>
2019-05-11 00:35:41 +0200

    Switched video output container to mp4
    for possibly better compatibility of players

--- container_scripts/start_recording.sh
+++ container_scripts/start_recording.sh
@@ -8,4 +8,4 @@ VIDEO_PREFIX=${VIDEO_PREFIX:-xfce-test_video_}
 # assure file is existing and there is at least a newline
 echo "" > ${OVERLAY_FILE}
 
-ffmpeg -y -r 30 -f x11grab -s ${RESOLUTION} -i ${DISPLAY} -vf "drawtext=fontfile=Vera.ttf:textfile=${OVERLAY_FILE}:reload=1:fontcolor=white: fontsize=12: box=1: boxcolor=black@0.5:y=500" -c:v libx264 -f mpegts - 2>/data/${VIDEO_PREFIX}${DATE}.log > /data/${VIDEO_PREFIX}${DATE}.ts &
+ffmpeg -y -r 30 -f x11grab -s ${RESOLUTION} -i ${DISPLAY} -vf "drawtext=fontfile=Vera.ttf:textfile=${OVERLAY_FILE}:reload=1:fontcolor=white: fontsize=12: box=1: boxcolor=black@0.5:y=500" -c:v libx264 -movflags frag_keyframe+empty_moov -f mp4 - 2>/data/${VIDEO_PREFIX}${DATE}.log > /data/${VIDEO_PREFIX}${DATE}.mp4 &
