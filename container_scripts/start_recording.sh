e3cfa141a2c30571f51aa5e39de6f93ca53c6e4d
Florian Schüller <florian.schueller@gmail.com>
2019-05-11 23:21:58 +0200

    Changed video format, introduced container_scripts/record_interactive.sh
    Video format had problems at least in telegram

--- container_scripts/start_recording.sh
+++ container_scripts/start_recording.sh
@@ -8,4 +8,6 @@ VIDEO_PREFIX=${VIDEO_PREFIX:-xfce-test_video_}
 # assure file is existing and there is at least a newline
 echo "" > ${OVERLAY_FILE}
 
-ffmpeg -y -r 30 -f x11grab -s ${RESOLUTION} -i ${DISPLAY} -vf "drawtext=fontfile=Vera.ttf:textfile=${OVERLAY_FILE}:reload=1:fontcolor=white: fontsize=12: box=1: boxcolor=black@0.5:y=500" -c:v libx264 -movflags frag_keyframe+empty_moov -f mp4 - 2>/data/${VIDEO_PREFIX}${DATE}.log > /data/${VIDEO_PREFIX}${DATE}.mp4 &
+ffmpeg -y -r 30 -f x11grab -s ${RESOLUTION} -i ${DISPLAY} -vf "drawtext=fontfile=Vera.ttf:textfile=${OVERLAY_FILE}:reload=1:fontcolor=white: fontsize=12: box=1: boxcolor=black@0.5:y=500" -c:v libx264 -pix_fmt yuv420p -movflags frag_keyframe+empty_moov -f mp4 - 2>/data/${VIDEO_PREFIX}${DATE}.log > /data/${VIDEO_PREFIX}${DATE}.mp4 &
+
+echo "Started recording to /data/${VIDEO_PREFIX}${DATE}.mp4"
