0c71c640028a20ba9c073799327b37670876be8a
Florian Schüller <florian.schueller@gmail.com>
2019-05-12 23:40:38 +0200

    Minor video helpers

--- container_scripts/start_recording.sh
+++ container_scripts/start_recording.sh
@@ -9,5 +9,6 @@ VIDEO_PREFIX=${VIDEO_PREFIX:-xfce-test_video_}
 echo "" > ${OVERLAY_FILE}
 
 ffmpeg -y -r 30 -f x11grab -s ${RESOLUTION} -i ${DISPLAY} -vf "drawtext=fontfile=Vera.ttf:textfile=${OVERLAY_FILE}:reload=1:fontcolor=white: fontsize=12: box=1: boxcolor=black@0.5:y=500" -c:v libx264 -pix_fmt yuv420p -movflags frag_keyframe+empty_moov -f mp4 - 2>/data/${VIDEO_PREFIX}${DATE}.log > /data/${VIDEO_PREFIX}${DATE}.mp4 &
+date +%s > /tmp/video_start_time
 
 echo "Started recording to /data/${VIDEO_PREFIX}${DATE}.mp4"
--- container_scripts/stop_recording.sh
+++ container_scripts/stop_recording.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
 killall ffmpeg
+date +%s > /tmp/video_stop_time
 
 echo "Recording stopped."
+
+VIDEO_LEN=$(( $(cat /tmp/video_stop_time) - $(cat /tmp/video_start_time) ))
+VIDEO_TIME=$(date --utc --date=@${VIDEO_LEN} +%T)
+echo "The video has ${VIDEO_LEN} seconds (${VIDEO_TIME})"
