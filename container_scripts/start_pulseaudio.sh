d8efa636587c569e36bd0881cdf0201f507c4712
Florian Schüller <florian.schueller@gmail.com>
2019-08-03 19:41:46 +0200

    try to avoid skipping

--- container_scripts/start_pulseaudio.sh
+++ container_scripts/start_pulseaudio.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "load-module module-udev-detect tsched=0" |sudo tee /etc/pulse/default.pa
 pulseaudio -vv >/tmp/pulseaudio.log 2>&1 &
 sleep 3
 pactl load-module module-null-sink sink_name=XFCETestAudio sink_properties=device.description="XFCETestAudio"
