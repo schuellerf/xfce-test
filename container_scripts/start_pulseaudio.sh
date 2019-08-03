6017bb83b6d090bc677d1842cd60a351f7f5a552
Florian Schüller <florian.schueller@gmail.com>
2019-08-03 17:15:49 +0200

    Better debugging for pulseaudio
    additional local tag to rebuild easier

--- container_scripts/start_pulseaudio.sh
+++ container_scripts/start_pulseaudio.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pulseaudio -vv &
+pulseaudio -vv >/tmp/pulseaudio.log 2>&1 &
 sleep 3
 pactl load-module module-null-sink sink_name=XFCETestAudio sink_properties=device.description="XFCETestAudio"
 fg
