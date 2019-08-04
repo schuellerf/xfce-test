cfae44f480b446a85f41480b7ba6f2c41b4616df
Florian Schüller <schuellerf@users.noreply.github.com>
2019-08-04 17:11:55 +0200

    Update start_pulseaudio.sh

--- container_scripts/start_pulseaudio.sh
+++ container_scripts/start_pulseaudio.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "load-module module-udev-detect tsched=0" |sudo tee /etc/pulse/default.pa
-pulseaudio -vv >/tmp/pulseaudio.log 2>&1 &
+sudo pulseaudio -vv >/tmp/pulseaudio.log 2>&1 &
 sleep 3
-pactl load-module module-null-sink sink_name=XFCETestAudio sink_properties=device.description="XFCETestAudio"
+sudo pactl load-module module-null-sink sink_name=XFCETestAudio sink_properties=device.description="XFCETestAudio"
 fg
 
