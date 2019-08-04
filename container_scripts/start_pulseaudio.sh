bb321cb3267d5a8eb10d7c7aa3be358c485a0872
Florian Schüller <florian.schueller@gmail.com>
2019-08-04 20:00:39 +0200

    See if having a systemwide dbus helps within travis

--- container_scripts/start_pulseaudio.sh
+++ container_scripts/start_pulseaudio.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 echo "load-module module-udev-detect tsched=0" |sudo tee /etc/pulse/default.pa
-sudo pulseaudio -vv >/tmp/pulseaudio.log 2>&1 &
+pulseaudio -vv >/tmp/pulseaudio.log 2>&1 &
 sleep 3
-sudo pactl load-module module-null-sink sink_name=XFCETestAudio sink_properties=device.description="XFCETestAudio"
+pactl load-module module-null-sink sink_name=XFCETestAudio sink_properties=device.description="XFCETestAudio"
+pacmd list-sinks > /tmp/pulseaudio_sinks.log
 fg
 
