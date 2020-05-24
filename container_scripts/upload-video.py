e6b49a95ec767b9c8d1d0856c067fa5d2fd14954
Florian Schüller <florian.schueller@gmail.com>
2020-05-24 10:49:58 +0200

    Python3 exceptions

--- container_scripts/upload-video.py
+++ container_scripts/upload-video.py
@@ -136,13 +136,13 @@ def resumable_upload(insert_request):
         print("Video id '%s' was successfully uploaded." % response['id'])
       else:
         exit("The upload failed with an unexpected response: %s" % response)
-    except HttpError, e:
+    except HttpError as e:
       if e.resp.status in RETRIABLE_STATUS_CODES:
         error = "A retriable HTTP error %d occurred:\n%s" % (e.resp.status,
                                                              e.content)
       else:
         raise
-    except RETRIABLE_EXCEPTIONS, e:
+    except RETRIABLE_EXCEPTIONS as e:
       error = "A retriable error occurred: %s" % e
 
     if error is not None:
@@ -176,5 +176,5 @@ if __name__ == '__main__':
   youtube = get_authenticated_service(args)
   try:
     initialize_upload(youtube, args)
-  except HttpError, e:
+  except HttpError as e:
     print("An HTTP error %d occurred:\n%s" % (e.resp.status, e.content))
