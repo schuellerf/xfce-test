8e50b404dc85511264f96655ee40f579c71d25e3
Florian Schüller <florian.schueller@gmail.com>
2021-01-08 19:19:39 +0100

    Restructure TAG and MAIN_BRANCH apprearance
    trying to get more encapsuled

--- container_scripts/build_all-latest.sh
+++ container_scripts/build_all-latest.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+MAIN_BRANCH=${MAIN_BRANCH:-last_tag}
+
 # Just a wrapper call to be able to override if needed
 MY_DIR=$(dirname $(readlink -f $0))
 ${MY_DIR}/build_all.sh
--- container_scripts/build_all-ubuntu_18.04.sh
+++ container_scripts/build_all-ubuntu_18.04.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# fixing to xfce-4.14 mainly for garcon needing newer Automake
+# not available in ubuntu 18.04 (by default)
+MAIN_BRANCH=${MAIN_BRANCH:-xfce-4.14}
+
 # Just a wrapper call to be able to override if needed
 MY_DIR=$(dirname $(readlink -f $0))
 ${MY_DIR}/build_all.sh
